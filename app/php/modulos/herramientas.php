769bbef23d4594c89020cd044483117ef41b0f4e
David E Luna M <davidlunamontilla@gmail.com>
2020-11-12 20:40:22 -0400

    Ultima actualizacion

--- app/php/modulos/herramientas.php
+++ app/php/modulos/herramientas.php
@@ -72,21 +72,113 @@
 
 				<div>
 					<img src="https://i.ibb.co/NCQnvfY/image.png" alt="Material educactivo para pacientes desarrollo por la Función Venezolana para la Educación e Investigación contra el Cáncer de Mama.">
+					<p class="text text--justify"><i>Material educativo para pacientes, desarrollado por la Fundación Venezolana para la Educación e Investigación contra el Cáncer de Mama (FUVEICAM)</i></p>
 				</div>
 			</li>
 
 			<li class="text text--justify">
-				
+				<strong>Grado de Diferenciación Celular</strong>
+
+        <p class="text text--justify">
+          Existen tres grados histológicos dependiendo de cuán diferente son las células tumorales en relación a las células normales o sanas de la mama: 
+        </p>
+
+        <p class="text text--justify">
+            <strong>Grado 1:</strong> Bajo grado o bien diferenciado. Las células cancerosas lucen sólo un poco diferente a las células normales.
+        </p>
+
+        <p class="text text--justify">
+          <strong>Grado 2:</strong> Grado Intermedio o moderadamente diferenciado. Las células cancerosas no lucen como las células normales, ya que se reproducen un poco más rápido.
+        </p>
+
+        <p class="text text--justify">
+          <strong>Grado 3:</strong> Alto Grado o Pobremente diferenciado. Las células lucen muy diferente a las células normales, ya que se reproducen de rápida manera.
+        </p>
+
+        <p class="text text--justify">
+          El cáncer de mama tiene su propio sistema para determinar su capacidad y velocidad de crecimiento, y se denomina Sistema  de Gradación de Nottingham o  modificación de Elston-Ellis del sistema de gradación de Scarff-Bloom-Richardson.  En este caso las características que se evalúan incluyen la disposición normal de las células en los ductos (formación tubular), la evaluación del tamaño y la forma del núcleo (grado nuclear), y cuántas de las células cancerosas están en el proceso de división (índice mitótico). Se asigna  a cada categoría una puntuación del 1 al 3. 
+        </p>
+
+        <p class="text text--justify">
+          Si suman un total de 3 a 5, el cáncer es de grado 1 (bien diferenciado). Los carcinomas bien diferenciados tienen células de aspecto relativamente normal que no parecen estar creciendo rápidamente. Están ordenadas en forma de pequeños túbulos para el cáncer ductal y en forma de cordones para el cáncer lobulillar. Los carcinomas bien diferenciados tienden a crecer y a propagarse lentamente, y tienen un mejor pronóstico.
+        </p>
+
+        <p class="text text--justify">
+          Si suman en total 6 o 7 significa que el cáncer es de grado 2 (moderadamente diferenciado). Los carcinomas moderadamente diferenciados tienen características y un pronóstico intermedio entre estos dos tipos.
+        </p>
+
+        <p class="text text--justify">
+          Si suman en total 8 o 9 significa que el cáncer es de grado 3 (pobremente diferenciado). Los carcinomas pobremente diferenciados tienen células que carecen de características normales, tienden a crecer y a propagarse rápidamente, y tienen un pronóstico peor.
+        </p>
+
+        <p class="text text--justify"><strong><i>El Grado es completamente diferente al estadio</i></strong></p>
 			</li>
+
+      <li>
+        <strong> Otros Parámetros</strong>
+
+        <ol>
+          <li>
+            <strong>Índice de Proliferación Tumoral</strong>
+
+            <p class="text text--justify">
+              El reporte de la biopsia puede incluir la proporción de células cancerosas dentro del tumor que se está dividiendo y creciendo. La prueba más común y ampliamente utilizada es el Ki-67.
+            </p>
+
+            <p class="text text--justify">
+              El Ki-67 es una proteína celular que se incrementa cuando la célula se prepara para dividirse.  Un proceso de coloración permite determinar el porcentaje de las células tumorales que son positivas para el Ki-67, evaluando de esta manera qué tan rápido crecen y se dividen las células cancerosas. Valores altos significan que muchas células se están dividiendo rápidamente, por lo que potencialmente el tumor es más agresivo.
+            </p>
+
+            <p class="text text--justify">
+              En el cáncer de mama un resultado menor de 10% es considerado bajo, entre 10-20% es considerado intermedio y más de 20% es considerado alto.
+            </p>
+          </li>
+
+          <li>
+            <strong>Invasión Linfovascular o Angiolinfática</strong>
+
+            <p class="text text--justify">
+              Consiste en la presencia de células cancerosas en vasos sanguíneos pequeños o en los canales linfáticos de la muestra al microscopio. Si en el informe se reporta como presente, puede considerarse como un parámetro de mayor agresividad del tumor.  Si dicha invasión no se evidencia, el informe puede que no la mencione o se reporte como ausente. La invasión angiolinfática no es lo mismo que la afectación de los ganglios linfáticos.
+            </p>
+          </li>
+        </ol>
+      </li>
+
+      <!-- d. -->
+      <li>
+        <strong>Receptores Hormonales</strong>
+
+        <p class="text text--justify">
+          Los receptores hormonales son proteínas, que como toda proteína su producción es controlada por genes. Las células cancerosas pueden ser receptores de estrógeno positivas, si tienen receptores para el estrógeno y receptores de progesterona positivas, si tienen receptores para la progesterona, estas señales hormonales favorecen el crecimiento tumoral. Si las células tumorales no tienen receptores para ambas hormonas se denominan receptores hormonales negativas. 
+        </p>
+
+        <p class="text text--justify">
+          Los resultados de los receptores hormonales en la biopsia pueden expresarse o bien por porcentaje, es decir el número de células que tiene receptores en 100 células analizadas, que puede ir entre 0% (ninguna tiene receptor), hasta 100%  (todas tienen receptor); o bien según la escala de Allred, que analiza qué porcentaje de células dan positivo para los receptores hormonales, junto con qué tan adecuado se muestran los receptores después de la tinción (intensidad). Se clasifican en una escala de 0 a 8. Cuanto mayor sea la puntuación,  más receptores se encontraron y se identificaron más intensamente.
+        </p>
+
+        <p class="text text--justify">
+          El gen <strong>Her2</strong> es un gen que se encuentra normalmente en las células de la glándula mamaria. Es el responsable de codificar y producir proteínas HER2. Estas proteínas son receptores en las células mamarias, que  en circunstancias normales, ayudan a controlar cómo crece, se divide y se repara la célula de la mama, pero en ocasiones puede ocurrir que el gen HER2 haga demasiadas copias de sí mismo, es decir se amplifique. Cuando esto sucede, los receptores HER2  sobreexpresados  estimulan  a las células mamarias para crecer y dividirse de manera incontrolada. El cáncer de mama con gen HER2 amplificado o que sobreexpresa la proteína Her2 se define como HER-2 positivo. Se caracteriza por crecer más rápido y cuenta con tratamientos o terapias dirigidas.
+        </p>
+
+        <p class="text text--left"><strong>Existen  cuatro pruebas para el HER2:</strong></p>
+
+        <ul>
+          <li>
+            <p class="text text--justify">
+            Análisis inmunohistoquímico (IHQ): Indica si hay demasiada proteína HER2 en las células cancerígenas. Los resultados del análisis de IHQ pueden ser: 0 (negativo), 1+ (también negativo), 2+ (ambiguo), o 3+ (positivo - sobreexpresión de la proteína HER2-).
+            </p>
+          </li>
+        </ul>
+      </li>
 		</ol>
 </div>
 
 HTML;
 
 
-		$sidebar = <<<HTML
+	$sidebar = <<<HTML
 <div class="sidebar__item">
-		<h2>Sidebar</h2>
+	<h2>Sidebar</h2>
 </div>
 HTML;
 		}
