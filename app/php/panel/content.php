79c2f4f495f7cb40ff8f3b6c967f2b58e12b0f75
David E Luna M <davidlunamontilla@gmail.com>
2020-12-06 04:00:29 -0400

    Optimizacion del formulario de inicio de sesion

--- app/php/panel/content.php
+++ app/php/panel/content.php
@@ -13,16 +13,22 @@ $form = <<<HTML
   <div class="sesion__content">
     <div class="sesion__logo" data-src="../multimedia/vectores/logotipo-slogan.svg"></div>
     
+    <hr>
+
     <form class="sesion__form" action="" method="post">
       <label class="sesion__label" for="usuario">
-        <span>Usuario:</span>
-        <input type="text" name="usuario" id="usuario">
+        <span class="sesion__caption">Usuario:</span>
+        <input class="sesion__input" type="text" name="usuario" id="usuario">
       </label>
 
       <label class="sesion__label" for="password">
-        <span>Contraseña:</span>
-        <input type="password" name="password" id="password">
+        <span class="sesion__caption">Contraseña:</span>
+        <input class="sesion__input" type="password" name="password" id="password">
       </label>
+
+      <div class="group-button">
+        <button type="submit" class="button button--success">Iniciar Sesión</button>
+      </div>
     </form>
 
   </div>
