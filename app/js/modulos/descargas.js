79f3882a9a23c4b0e5b0fa54eeca3872cdf0bcdf
David E Luna M <davidlunamontilla@gmail.com>
2021-01-16 05:08:25 -0400

    Implementacion parcial de las tarjetas de descargas en la ventana modal

--- app/js/modulos/descargas.js
+++ app/js/modulos/descargas.js
@@ -29,14 +29,113 @@ const descargas = () => {
 
 
     const documentos = ( data ) => {
+        if ( ! ("header" in data) ) return;
+
         let { icon, headerTitle } = data.header;
-        // let { icons, title, caption, href } = cuerpo;
+        if ( isNull( modalContent ) )
+            return;
 
-        for ( let cuerpo of data.cuerpo ) {
-            let { icons, title, caption, href } = cuerpo;
+        let documento = div.cloneNode( false ),
+            divIcon = div.cloneNode( false ),
+            divTitle = div.cloneNode( false );
 
+        // Implementación el ícono y título:
+        divIcon.classList.add( "documentos__icon" );
+        divTitle.classList.add("documentos__title" );
+
+        // Implementación de ícono SVG y cadena en el título:
+        divIcon.setAttribute("data-src", icon );
+        divTitle.textContent = headerTitle;
+        
+        // Implementación de la cabecera de la tarjeta:
+        cabecera.innerHTML  = "";
+        cabecera.append( divIcon, divTitle );
+        
+        // Si el elemento a evaluar no es un array no continúa:
+        if ( ! (Array.isArray( data.cuerpo )) )
+            return;
+
+        // Implementar las tarjetas en el cuerpo del documento:
+        let cuerpo = seccion.cloneNode( false );
+        for ( let datos of data.cuerpo ) {
+            let { icons, title, caption, href } = datos;
+
+            // Si todos estos elementos no están definidos debe
+            // saltárselo y seguir:
+            if (
+                typeof icons == "undefined" ||
+                typeof title == "undefined" ||
+                typeof caption == "undefined" ||
+                typeof href == "undefined"
+            ) {
+                continue;
+            }
+
+            // Elementos de la tarjeta:
+            let tarjeta = enlace.cloneNode( false ),
+
+                // «contenedorGrafico» contiene a «identificador» y «tituloTarjeta»
+                contenedorGrafico = div.cloneNode( false ),
+                identificador = div.cloneNode( false ),
+                tituloTarjeta = div.cloneNode( false ),
+
+
+                // «descripcion» contiene a a «leyenda« e «iconoDescarga»
+                descripcion = div.cloneNode( false ),
+                leyenda = div.cloneNode( false ),
+                iconoDescarga = div.cloneNode( false );
+
+            // Implementación de clases:
+            tarjeta.classList.add( "tarjetas__item" );
             
+            // Aplicar clases al contenedor gráfico con sus elementos:
+            contenedorGrafico.classList.add("tarjetas__contenedor-grafico");
+            identificador.classList.add("tarjetas__identificador");
+            tituloTarjeta.classList.add("tarjetas__titulo");
+
+            // Aplicar clases a la descripción de la tarjeta 
+            // con sus elementos:
+            descripcion.classList.add("tarjetas__descripcion");
+            leyenda.classList.add("tarjetas__leyenda");
+            iconoDescarga.classList.add("tarjetas__descarga");
+
+            // Implementar contenido de la tarjeta:
+            if ( "identificador" in icons )
+                identificador.setAttribute( "data-src", icons.identificador);
+
+            if ( "descarga" in icons )
+                iconoDescarga.setAttribute("data-src", icons.descarga );
+            
+            // Incorporar la url al enlace:
+            tarjeta.setAttribute("href", href);
+
+            // Agregando contenido:
+            tituloTarjeta.textContent = title;
+            contenedorGrafico.append( identificador, tituloTarjeta );
+
+            leyenda.textContent = caption;
+            
+            // Se incorporarán leyendas y descargas en el área de descripción:
+            descripcion.append( leyenda, iconoDescarga );
+
+            // Incorporar todos estos elementos al enlace:
+            tarjeta.append( contenedorGrafico, descripcion );
+            
+            cuerpo.appendChild( tarjeta );
         }
+
+        // Implementación de clases:
+        documento.classList.add( "documentos" );
+        cabecera.classList.add("documentos__header");
+        cuerpo.classList.add("documentos__cuerpo", "tarjetas");
+
+        // Incorporar los elementos al documento:
+        documento.append( cabecera, cuerpo );
+
+        // Limpiar contenido de la Ventana Modal antes de ingresar:
+        modalContent.innerHTML = "";
+
+        return documento;
     };
 
     iframe.classList.add("modal__document");
@@ -58,17 +157,44 @@ const descargas = () => {
         enlace.addEventListener("click", function (e) {
             if (this.classList.contains(vista)) {
                 e.preventDefault();
-
-                // Abrir la ventana modal y mostrar el documento
-                // PDF en ella:
-                iframe.setAttribute("src", this.href);
-                modalContent.appendChild(iframe);
+                const isJSON = /(\.json)/ig;
 
                 // Obtener título para la ventana Modal:
                 modalTitle.textContent = (typeof this.dataset.title !== "undefined")
                     ? this.dataset.title : "";
 
+                // Limpiar el contenedor de la ventana modal:
+                // modalContent.removeChild();
+                modalContent.innerHTML = "";
+
+                // Abrir la ventana modal:
                 modal.classList.add("modal--show");
+
+
+                // Cargar el archivo JSON en la ventana modal
+                // si lo es:
+                if ( isJSON.test( this.href ) ){
+                    // Vaciar elementos:s
+                    modalContent.innerHTML = "";
+
+                    fetch( this.href, {
+                        cache: "no-store",
+                        pragma: "no-cache"
+                    })
+                    .then( respuesta => respuesta.json() )
+                    .then( data => {
+                        modalContent.appendChild( documentos( data ) );
+                    });
+
+                    return;
+                }
+
+                // Cargar un documento en formato PDF en la ventana modal:
+                iframe.setAttribute("src", this.href);
+                modalContent.appendChild(iframe);
+
+
+
             }
         }, false);
     })
